039c442827b5d8e4a20e37f4b7ce6eacd01eb8d1
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-01-19 22:39:32 +0630

    feat: ApexChart - PieChart and MixedChart

---
+++ DotNetTrainingBatch5.ChartWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetTrainingBatch5.ChartWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
